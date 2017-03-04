34940e54ad64891f469c0322d0a86b9d49c9806d
Kenny Jacoby <kjacoby94@gmail.com>
2017-03-04 15:25:52 -0500

    Added table.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import csv
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
@@ -5,7 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	template = "index.html"
-	return render_template(template)
+	object_list = get_csv()
+	return render_template(template, object_list=object_list)
+
+def get_csv():
+	csv_path = "static/la-riots-deaths.csv"
+	csv_file = open(csv_path, "rb")
+	csv_obj = csv.DictReader(csv_file)
+	csv_list = list(csv_obj)
+	return csv_list
 
 # If this script is run from the command line
 if __name__ == "__main__":
